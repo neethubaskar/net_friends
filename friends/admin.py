f59a052cf18b47f5839682933fb0288851262e4c
neethubaskar <neethubaskar985@gmail.com>
2024-06-02 18:50:46 +0530

    friend request model registered in admin

--- friends/admin.py
+++ friends/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FriendRequest
+
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'status', 'created_on')
+    search_fields = ['from_user', 'to_user']
+    list_filter = ['status']
+
+    def from_user(self, obj):
+        return obj.created_by
+
+    from_user.short_description = "From User"
+
+
+admin.site.register(FriendRequest, FriendRequestAdmin)
